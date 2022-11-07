d76119798aedc7f6b5f3a208549c397f47733a0a
Shubham-Jambhale <40384748+Shubham-Jambhale@users.noreply.github.com>
2022-11-07 01:36:17 -0500

    added few changes

--- Dynamic_Programming_Subset_Sum_Divisible.py
+++ Dynamic_Programming_Subset_Sum_Divisible.py
@@ -1,30 +1,66 @@
-def get_max_sbstr (s , c1 , c2 ):
-    
-    #creating a substring for performing operation for matrix 
-    substring =c1+c2
-    new_string1 = c1 + s
-    #calling 2 times because checking if adding c1 giving more or c2 giving more
-    get_how_many_substring_c1 = get_how_many_string(new_string1,substring)
-    new_string2 = s + c2
-    get_how_many_substring_c2 = get_how_many_string(new_string2,substring)
-    
-    if get_how_many_substring_c1 > get_how_many_substring_c2:
-        return get_how_many_substring_c1
-    else:
-        return get_how_many_substring_c2
+def subset_divisible ( A  , target ):
+    sum = [[0 for j in range(target+1)] for i in range(len(A)+1)]
+    parent =[[0 for j in range(target+1)]for i in range(len(A)+1)]
+    array =[]
+    remainder = []
+    array_rem = []
+    final_array =[]
+    #finding the remainder and storing them in an array also passing the remainder array to subset sum 
+    for i in range(len(A)):
+        array_rem_append = []
+        rem = A[i] % target
+        array_rem_append.append(A[i])
+        if rem == 0:
+            rem = rem + target 
+        array_rem_append.append(rem)
+        array_rem.append(array_rem_append)
+        remainder.append(rem)
 
-def get_how_many_string(s,substring):
+    subset,parent = subset_sum(remainder,target,sum,parent)
+    #if subset exists find parents else return empty list
+    if subset:
+        abc = get_subset(len(A),target,parent,array)
+    else:
+        return []
+
+    for i in range(len(abc)):
+        j = 0
+        while j < len(array_rem):
+            if array_rem[j][1] == abc[i] :
+                final_array.append(array_rem[j][0])
+                array_rem.pop(j)
+                break
+            j =j+1
+    return final_array
+
+##Referred from professors notes
+def subset_sum(A,k,sum,parent):    
     
-    matrix = [[0 for i in range(len(substring)+1)]for i in range(len(s)+1)]
+    sum[0][0]=True
+    parent[0][0] = None
     
-    for i in range(len(matrix)):
-        matrix[i][0] = 1
+    for i in range(1,k+1):
+        sum[0][i] = False
+        parent[0][i] = None
     
-    for i in range(1,len(matrix)):
-        for j in range(1,len(matrix[0])):
-            if s[i-1] == substring[j-1]:
-                matrix[i][j] = matrix[i-1][j-1] + matrix[i-1][j]
-            else:
-                matrix[i][j] = matrix[i-1][j]
-        
-    return matrix[-1][-1]
+    for i in range(1, len(A)+1):
+        for j in range(k+1):
+            sum[i][j] =sum[i-1][j]
+            parent[i][j] =None
+            
+            if j >=A[i-1] and sum[i-1][j-A[i-1]] == True:
+                sum[i][j] =True
+                parent[i][j] = j-A[i-1]
+#     print(sum)
+                
+    return sum[len(A)][k],parent
+
+def get_subset(len_A,k,parent,array):
+    if k == 0:
+        return 
+    if parent[len_A][k] ==None:
+        get_subset((len_A-1),k,parent,array)
+    else:
+        get_subset(len_A-1,parent[len_A][k],parent,array)
+        array.append(k-parent[len_A][k])        
+    return array
